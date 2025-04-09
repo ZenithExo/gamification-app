092796452f25c2e96e143e49721e050d291012b8
ZenithExo <zenithexodus55@gmail.com>
2025-04-09 17:12:35 +0530

    Changes in app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
-app.listen()
+app.listen(
+    PORT, 
+    console.log('Server running in ${process.env.NODE_ENV} mode on port ${PORT}')
+)
